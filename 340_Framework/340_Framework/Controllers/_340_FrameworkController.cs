80d534c7d645f5a19248d61951997ec0301806f7
Kate Sanders <katesandersm@gmail.com>
2018-03-24 01:20:31 -0500

    Finished part one of https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro tutorial

---
+++ 340_Framework/340_Framework/Controllers/_340_FrameworkController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _340_Framework.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _340_Framework.Controllers
+{
+    public class _340_FrameworkController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public _340_FrameworkController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // GET: _340_Framework
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Organization.ToListAsync());
+        }
+
+        // GET: _340_Framework/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: _340_Framework/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: _340_Framework/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: _340_Framework/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: _340_Framework/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: _340_Framework/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: _340_Framework/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- 340_Framework/340_Framework/Program.cs
+++ 340_Framework/340_Framework/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using _340_Framework.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace _340_Framework
@@ -14,7 +16,26 @@ namespace _340_Framework
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
