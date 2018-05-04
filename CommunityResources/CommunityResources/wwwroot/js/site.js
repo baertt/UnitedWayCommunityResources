5356f37461875f974f0912a5f16b04382b90d1af
Kate Sanders <SandersKM@Hendrix.edu>
2018-05-04 10:25:43 -0500

    Dropdown for times started

--- CommunityResources/CommunityResources/wwwroot/js/site.js
+++ CommunityResources/CommunityResources/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿function myFunction() {
+﻿
+
+function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
@@ -23,3 +25,78 @@ window.onclick = function (event) {
         }
     }
 };
+
+$(document).ready(function () {
+
+
+    for (var i = 700; i <= 1700; i += 15) {
+        var mins = i % 100;
+        var hours = parseInt(i / 100);
+
+        if (mins > 45) {
+            mins = 0;
+            hours += 1;
+            i = hours * 100;
+        }
+
+        //Logic that converts hours to standard time am/pm
+        var standardTime = ' AM';
+
+        if (hours > 12) {
+            standardTime = ' PM';
+            hours %= 13;
+            hours++;
+        } else {
+            hours %= 13;
+        }
+
+        $('#stime').append('<option value="' + i + '">' + ('0' + (hours)).slice(-2) + ':' + ('0' + mins).slice(-2) + standardTime + '</option>');
+    }
+
+    $('#stime').on('change', function () {
+        console.log("just is just a test");
+        var meetingLength = 30;
+        var selectedTime = Number($('#stime').val());
+        var sum = meetingLength + selectedTime;
+
+        $("#etime").html("<option value=\"\">--Select end time--</option>");
+
+        for (var i = sum; i <= 1700; i += meetingLength) {
+            var mins = i % 100;
+            var hours = parseInt(i / 100);
+
+            if (mins > 59) {
+                mins = 0;
+                hours += 1;
+                i = hours * 100;
+            }
+
+            //Logic that converts hours to standard time am/pm
+            var standardTime = ' AM';
+
+            if (hours > 12) {
+                standardTime = ' PM';
+                hours %= 13;
+                hours++;
+            } else {
+                hours %= 13;
+            }
+            console.log("we are trying to add more times i =" + i + "");
+            $('#etime').append('<option value="' + i + '">' + ('0' + (hours)).slice(-2) + ':' + ('0' + mins).slice(-2) + standardTime + '</option>');
+        }
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
